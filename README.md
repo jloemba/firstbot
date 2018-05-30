9a4e8f806daa5737cedb2104a796f000ee50fad9
Joris Loemba <jorisloemba@gmail.com>
2018-05-30 21:55:43 +0100

    README.md modifié

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
-1) Appeler le module **BOTBUILDER**
-  Permet de créer le bot et de gérer les interactions avec le bot depuis le serveur(voir suite).
+Notions à comprendre : BOTBUILDER,SERVICE WEB ,CHATCONNECTOR , UNIVERSALBOT, UNITE DE STOCKAGE , PROMPT  
 
+1) Appeler le module **BOTBUILDER** qui permet de créer le bot et de gérer les interactions avec le bot depuis le service web(voir suite).  
+  Appeler le module **SERVICE WEB** via *restify*
+  qui permet de créer le service web afin de partager les messages entre le bot et le client. On l'utilisera sous forme de serveur.
 
+  ````
+  var builder = require("botbuilder");
+  var restify = require("restify");
+  ````
 
-2) Appeler le **RESTIFY**
-  Permet de créer le serveur REST afin de partager les messages entre le bot et le client
-
-3) Configurer et Lancer ce serveur pour le rendre disponible sur notre client(*Bot Emulator* mettre un lien vers le page de download du SDK)
+2) Configurer et Lancer ce serveur pour le rendre disponible sur notre client(*Bot Emulator* mettre un lien vers le page de download du SDK)  
+Chaque serveur doit avoir un port d'écoute pour fonctionner.
 
 ````
 var server = new restify.createServer();
@@ -14,35 +18,49 @@ const port = process.env.PORT || 3978;
 
 server.listen(port, function(){console.log('Serveur sur écoute du port ${port} du serveur ${server.name}');});
 ````
-4) Créer une instance d'un **CHATCONNECTOR** depuis le module BotBuilder.
+
+4) Créer une instance d'un **CHATCONNECTOR** depuis le module BotBuilder.  
+Le ChatConnector connecte le bot au serveur afin de "communiquer" avec lui.
 ````
 var connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
     appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 ````
-5) Soumettre l'instance du BotBuilder à notre serveur REST via une *requête POST*.
+
+5) Soumettre l'instance du BotBuilder à notre serveur REST via une *requête POST*.  
+On lui dit que le connecteur écoutera les évènements à cette requête.
 ````
 server.post("/api/messages", connector.listen());
 ````
 
 6) Instancier une **UNITE DE STOCKAGE** depuis le BotBuilder
-(Expliquer)
+La conversation avec le bot nécessitera une persistance de certaines infos pour le tracking de cette dernière.  
 ````
 var inMemoryStorage = new builder.MemoryBotStorage();
 ````
 
+Ces infos sont récupérés depuis des **STATES**  
+- Les infos de l'utilisateur que l'on peut récupérer via le state *UserData*  
+- ceux de la conversation via *conversationData*  
+- ceux des dialogues via *dialogData*  
+
 7) Créer une instance d'un **UNIVERSALBOT**
   -  Y associer le connector , l'unité de stockage et le tableau de fonctions(voir plus loin)
+  - On appelle le root (premier dialogue) avec beginDialog(nom du dialogue) .
+
 ````
 var bot = new builder.UniversalBot(connector, [
     function(session){
         session.beginDialog("menu");
     }
 ]).set('storage', inMemoryStorage);
-````
+````  
 
-8) Créer un dialogue avec la méthode dialog de l'instance UniversalBot prenant 2 paramètres : le nom du dialogue et un tableau de fonction.
+C'est l'objet qui modalise une conversation. Cette conversation empilera un ou plusieurs dialogue. On parlera de **PILE DE DIALOGUE** qui est visible sur le client. Par défaut , la pile de dialogue est vide  . Si l'utilisateur envoie un message, le premier dialogue lui sera envoyé.
+
+
+8) Créer un dialogue avec la méthode dialog(nom du dialogue,tableau de fonction) de l'instance UniversalBot prenant 2 paramètres.
 
   - Appeler un **PROMPT** dans une fonction pour afficher au client via la méthode *text()*.
 
--- app.js
+++ app.js
@@ -1,57 +1,122 @@
-// 1 ) Appel du connecteur ( on s'en sert gèrer les intéractions avec le bot ) . Il faudra le configurer plus tard.
-var builder = require('botbuilder');
+require('dotenv').config();//Module dédié à la gestion de variables d'environnement(fichier .env)
+
+// 1 ) Appel du connecteur => Gère/Programme les interactions entre le Bot et le client .
+var builder = require("botbuilder");
 
 //Sauf que l'on a pas de module pour diffuser les messages à tous les intéragisseurs , on va appeler le module 'restify'
-//2) Appeler un serveur RESTIFY pour broacast les messages
-var restify = require('restify');
-var server = restify.createServer();
-const port = 3978;
+//2) Appeler un serveur RESTIFY pour broacast les messages => Envoyer des messages au BOT / d'en recevoir
+var restify = require("restify");
 
 //Chaque serveur doit avoir un port d'écoute pour fonctionner .
-//3) On lui en donne un "au hasard" qui n'est pas utilisé.
-server.listen(port,function(){console.log("Serveur sur écoute du port "+port+" du serveur",server.name);});
+//3) On crée un serveur en lui donnant un port d'écoute libre.
+var server = new restify.createServer();
+const port = process.env.PORT || 3978;
 
-// 4) Configuration de notre BUILDER via ChatConnector()
+server.listen(port, function(){console.log(`Serveur sur écoute du port ${port} du serveur ${server.name}`);});
+
+// 4) Configuration de notre BUILDER pour connecter le client avec bot.
 //Par défaut 'ChatConnector' n'attend pas de paramètres
-var connector = new builder.ChatConnector();
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
 
-// 5)   Créer notre route de dialogue avec une requête POST
-//Pour envoyer un message au BOT , on créer un requête POST.
+// 5)   Créer notre route de dialogue avec une requête POST pour ENVOYER un message
 //Dans celle-ci ,on dit que le BUILDER doit écouter les évènement à cette requête.
-//A chaque message , le serveur se rafraîchit et charge automatiquement les messages connector.listen
-server.post('/api/messages' ,connector.listen());
+server.post("/api/messages", connector.listen());
 
-// 6) Ouverture du manageur de conversation entre l'utilisateur et le bot.
-// Manages your bots conversations with users across multiple channels
+// creation d'un unite de stockage
+var inMemoryStorage = new builder.MemoryBotStorage();
 
-//On y met nos dialogues. Ces dialogues seront définis plus bas.
-var bot = new builder.UniversalBot(connector,[
-    //C'est ici que le dialogue commence, mais il faut l'appeler
+// 6) Ouverture de la conversation.
+//On y met nos dialogues sous forme de tableau de fonctions. Ces dialogues seront définis plus bas.
+var bot = new builder.UniversalBot(connector, [
     function(session){
-        // 7) Appel le dialogue (On appelle le 'beginDialogue' -> ROOT car c'est lui qui le commence )
-        session.beginDialog('dialog1');
-        session.beginDialog('dialog2'); //Trouver la bonne méthode pour avoir un 2nd exo.
-    },function(session,result){ //Une fois la première fonction terminée.
-        session.send("Coucou %s",result.response);
-        session.send("Je crois qu'elle peeut demain");
-    }//La pile est vide
-]);
+        //session.beginDialog("greetings", session.userData.profile);
+        //Cette fonction contient le dialogue "menu" programmé plus bas.
+        session.beginDialog("menu");
+    },
+    /*function(session, results){
+        session.userData.profile = results.response;
+        session.send(`hello ${session.userData.profile.name} :) !!`);
+    }*/
+]).set('storage', inMemoryStorage);
+//Définition de la mémoire de stockage pour nos DialogData , userData ou encore nos ConversionData.
 
+// ------   LES DIALOGUES DE NOTRE BOT ----- //
 
-//Un dialogue crée que l'on va implémenter dans le manager de concversion.
-bot.dialog('dialog1', [
-    function(session){// Notre LIFO car c'est notre première fonction
-        //Notre Bot utilise bot.Prompts.text() par affihcer c'est message sur l a console.
-        builder.Prompts.text(session, 'c\'est quoi ton prénom ?' );
+//Le 'dialog1' contient deux fonctions. La première demande le nom du User et le 2nd contient
+//la nom saisie qui sera retourner dans la conversation pour terminer le dialogue.
+bot.dialog("dialog1", [
+    function(session){
+        builder.Prompts.text(session,`c\'est quoi ton prénom ?`);
     },
     function(session, result){
-            session.endDialogWithResult(result);
+        session.endDialogWithResult(result);
     }
 ]);
 
-/*
-  NOTIONS à REVOIR  -> Builder  , beginDialog & Pile
 
-  ROOT  => dialogue par défaut de la conversation. Il se lancera automatiquement
-  LIFO => Première fonction d'un digolgue
+/* Un objet initialisant le menu de notre dialogue suivant. Celui contient le contient la valeurs des rubriques de cemnu*/
+const menuItems = {
+    "toto":{
+        items: "option1"
+    },
+    "titi":{
+        items: "option2"
+    },
+    "tutu":{
+        items: "option3"
+    }
+};
+
+/*Celui effectue :
+    - l'affichage du menu (menuItems) , l'obtention de la rubrique choisie.
+    - Redirection vers un dialogue selon le lien du menu choisis.
 */
+bot.dialog('menu',[
+    //Etape1
+    function(session){
+        builder.Prompts.choice(session,"Choisissez une option du menu suivant:",menuItems,{listStyle:3});
+        //Le prompt choisi affiche le lien du menu avec une mise en forme via l'attribut 'listStyle'.
+    },
+    function(session, results){
+        var choice = results.response.entity;
+        //Lien du menu relevé , si clique sur 'toto' , 'toto' sera stocké ici.
+        //Pour ensuite être redirigé vers le nom du dialogue associé la rubrique 'toto' via 'menuItems[choice].items'
+        session.beginDialog(menuItems[choice].items);
+    }
+]);
+
+
+/*Ce dialogue  :
+    1)  Définit les données de l'utilisateur en lui demandant son prénom
+    2)  Assigne la valeur saisie à l'attribut 'name' de 'profile' puis la retourne en tant que réponse dans la conversation.
+*/
+bot.dialog('greetings', [
+    //Etape 1
+    function (session, args, next){
+        session.dialogData.profile = args || {};
+        if(!session.dialogData.profile.name) builder.Prompts.text(session, "What's your name ?");
+        else next();
+    },
+    //Etape 2
+    function (session, result){
+        if(result.response){
+            session.dialogData.profile.name = result.response;
+        }
+        session.endDialogWithResult({response: session.dialogData.profile});
+    }
+]);
+
+
+/*--- Les dialogues de notre menu 'toto', 'titi' & 'tutu' ---*/
+bot.dialog('option1',[
+        function(session){ session.send('Vous êtes dans le dialogue 1');}
+]);
+bot.dialog('option2',[
+    function(session){ session.send('Vous êtes dans le dialogue 2');}
+]);
+bot.dialog('option3',[
+    function(session){session.send('Vous êtes dans le dialogue 3');}
+]);
